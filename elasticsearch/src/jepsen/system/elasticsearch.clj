ea16b817c454c9c07b853395171551da71d87f2b
Lee Hinman <lee@writequit.org>
2014-09-15 15:06:27 +0200

    Fix indenting and bad comments, log addition info about querying

--- elasticsearch/src/jepsen/system/elasticsearch.clj
+++ elasticsearch/src/jepsen/system/elasticsearch.clj
@@ -22,17 +22,17 @@
   [timeout-secs color]
   (timeout (* 1000 timeout-secs)
            (throw (RuntimeException.
-                    "Timed out waiting for elasticsearch cluster recovery"))
-    (loop []
-      (when
-        (try
-          (c/exec :curl :-XGET
-                  (str "http://localhost:9200/_cluster/health?"
-                       "wait_for_status=" (name color)
-                       "&timeout=" timeout-secs "s"))
-          false
-          (catch RuntimeException e true))
-        (recur)))))
+                   "Timed out waiting for elasticsearch cluster recovery"))
+           (loop []
+             (when
+                 (try
+                   (c/exec :curl :-XGET
+                           (str "http://localhost:9200/_cluster/health?"
+                                "wait_for_status=" (name color)
+                                "&timeout=" timeout-secs "s"))
+                   false
+                   (catch RuntimeException e true))
+               (recur)))))
 
 (defn primaries
   "Returns a map of nodes to the node that node thinks is the current primary,
@@ -60,82 +60,85 @@
 (def isolate-self-primaries-nemesis
   "A nemesis which completely isolates any node that thinks it is the primary."
   (nemesis/partitioner
-    (fn [nodes]
-      (let [ps (self-primaries nodes)]
-        (nemesis/complete-grudge
-          ; All nodes that aren't self-primaries in one partition
-          (cons (remove (set ps) nodes)
-                ; Each self-primary in a different partition
-                (map list ps)))))))
+   (fn [nodes]
+     (let [ps (self-primaries nodes)]
+       (nemesis/complete-grudge
+        ;; All nodes that aren't self-primaries in one partition
+        (cons (remove (set ps) nodes)
+              ;; Each self-primary in a different partition
+              (map list ps)))))))
 
 (def db
   (reify db/DB
     (setup! [_ test node]
       (c/su
-        (c/cd "/tmp"
-              (let [version "1.4.0-SNAPSHOT"
-                    debfile (str "elasticsearch-" version ".deb")
-                    uri     (str "http://writequit.org/files/"
-                                 debfile)]
-                (when-not (debian/installed? :elasticsearch)
-                  (loop []
-                    (info node "downloading elasticsearch")
-                    (c/exec :wget :-c uri)
-                    (c/exec :wget :-c (str uri ".sha1.txt"))
-                    (when (try
-                            (c/exec :sha1sum :-c (str debfile ".sha1.txt"))
-                            false
-                            (catch RuntimeException e
-                              (info "SHA failed" (.getMessage e))
-                              true))
-                      (recur)))
+       (c/cd "/tmp"
+             (let [version "1.4.0-SNAPSHOT"
+                   debfile (str "elasticsearch-" version ".deb")
+                   uri     (str "http://writequit.org/files/"
+                                debfile)]
+               (when-not (debian/installed? :elasticsearch)
+                 (loop []
+                   (info node "downloading elasticsearch")
+                   (c/exec :wget :-c uri)
+                   (c/exec :wget :-c (str uri ".sha1.txt"))
+                   (when (try
+                           (c/exec :sha1sum :-c (str debfile ".sha1.txt"))
+                           false
+                           (catch RuntimeException e
+                             (info "SHA failed" (.getMessage e))
+                             true))
+                     (recur)))
 
-                  (info node "installing elasticsearch")
-                  (c/exec :apt-get :install :-y :--force-yes :openjdk-7-jre-headless)
-                  (c/exec :dpkg :-i :--force-confnew debfile))))
+                 (info node "installing elasticsearch")
+                 (c/exec :apt-get :install :-y :--force-yes :openjdk-7-jre-headless)
+                 (c/exec :dpkg :-i :--force-confnew debfile))))
 
-        (info node "configuring elasticsearch")
-        (c/exec :echo
-                (-> "elasticsearch/default"
-                    io/resource
-                    slurp)
-                :> "/etc/default/elasticsearch")
-        (c/exec :echo
-                (-> "elasticsearch/elasticsearch.yml"
-                    io/resource
-                    slurp
-                    (str/replace "$NAME"  (name node))
-                    (str/replace "$HOSTS" (json/generate-string
-                                            (vals (c/on-many (:nodes test)
-                                                             (net/local-ip))))))
-                :> "/etc/elasticsearch/elasticsearch.yml")
+       (info node "configuring elasticsearch")
+       (c/exec :echo
+               (-> "elasticsearch/default"
+                   io/resource
+                   slurp)
+               :> "/etc/default/elasticsearch")
+       (c/exec :echo
+               (-> "elasticsearch/elasticsearch.yml"
+                   io/resource
+                   slurp
+                   (str/replace "$NAME"  (name node))
+                   (str/replace "$HOSTS" (json/generate-string
+                                          (vals (c/on-many (:nodes test)
+                                                           (net/local-ip))))))
+               :> "/etc/elasticsearch/elasticsearch.yml")
 
-        (info node "starting elasticsearch")
-        (c/exec :service :elasticsearch :restart))
+       (info node "starting elasticsearch")
+       (c/exec :service :elasticsearch :restart))
 
       (wait 60 :green)
       (info node "elasticsearch ready"))
 
     (teardown! [_ test node]
       (c/su
-        (meh (c/exec :service :elasticsearch :stop))
-        (c/exec :rm :-rf "/var/lib/elasticsearch/elasticsearch"))
+       (meh (c/exec :service :elasticsearch :stop))
+       (c/exec :rm :-rf "/var/lib/elasticsearch/elasticsearch"))
       (info node "elasticsearch nuked"))))
 
 (defn http-error
   "Takes an elastisch ExInfo exception and extracts the HTTP error response as
   a string."
   [ex]
-  ; AFIACT this is the shortest path to actual information about what went
-  ; wrong
+  ;; AFIACT this is the shortest path to actual information about what went
+  ;; wrong
   (-> ex .getData :object :body json/parse-string (get "error")))
 
 (defn all-results
   "A sequence of all results from a search query."
   [& search-args]
-  (let [res      (apply esd/search (concat search-args [:size 0]))
+  (let [_ (log/info "Querying results (size 0):" search-args)
+        res (apply esd/search (concat search-args [:size 0]))
         hitcount (esr/total-hits res)
-        res      (apply esd/search (concat search-args [:size hitcount]))]
+        _ (log/info "Querying results with hit-count:" hitcount)
+        res (apply esd/search (concat search-args [:size hitcount]))]
+    (log/info "raw response:" res)
     (when (esr/timed-out? res)
       (throw (RuntimeException. "Timed out")))
     (esr/hits-from res)))
@@ -147,7 +150,7 @@
   (setup! [_ test node]
     (let [; client (es/connect [[(name node) 9300]])]
           client (es/connect (str "http://" (name node) ":9200"))]
-      ; Create index
+      ;; Create index
       (try
         (esi/create client index-name
                     :mappings {"number" {:properties
@@ -155,8 +158,8 @@
                                                 :store "yes"}}}}
                     :settings {"index" {"refresh_interval" "1"}})
         (catch clojure.lang.ExceptionInfo e
-          ; Is this seriously how you're supposed to do idempotent
-          ; index creation? I've gotta be doing this wrong.
+          ;; Is this seriously how you're supposed to do idempotent
+          ;; index creation? I've gotta be doing this wrong.
           (let [err (http-error e)]
             (when-not (re-find #"IndexAlreadyExistsException" err)
               (throw (RuntimeException. err))))))
@@ -164,33 +167,34 @@
       (CreateSetClient. client)))
 
   (invoke! [this test op]
-      (case (:f op)
-        :add (timeout 5000 (assoc op :type :info :value :timed-out)
-                (let [r (esd/create client index-name "number" {:num (:value op)})]
-                  (info "client:" client "op:" op "resp:" r)
-                  (cond (esr/ok? r)
-                        (assoc op :type :ok)
+    (case (:f op)
+      :add (timeout 5000 (assoc op :type :info :value :timed-out)
+                    (let [r (esd/create client index-name "number" {:num (:value op)})]
+                      (info "client:" client "op:" op "resp:" r)
+                      (cond (esr/ok? r)
+                            (assoc op :type :ok)
 
-                        (esr/timed-out? r)
-                        (assoc op :type :info :value :timed-out)
+                            (esr/timed-out? r)
+                            (assoc op :type :info :value :timed-out)
 
-                        :else
-                        (assoc op :type :info :value r))))
-        :read (try
-                (info "Waiting for recovery before read")
-                ; Elasticsearch lies about cluster state during split brain
-                ; woooooooo
-                (c/on-many (:nodes test) (wait 1000 :green))
-                (info "Elasticsearch reports green, but it's probably lying, so waiting another 200s")
-                (Thread/sleep (* 200 1000))
-                (info "Recovered; flushing index before read")
-                (esi/flush client index-name)
-                (assoc op :type :ok
-                       :value (->> (all-results client index-name "number")
-                                   (map (comp :num :_source))
-                                   (into (sorted-set))))
-                (catch RuntimeException e
-                  (assoc op :type :fail :value (.getMessage e))))))
+                            :else
+                            (assoc op :type :info :value r))))
+      :read (try
+              (info "Waiting for recovery before read")
+              ;; Elasticsearch lies about cluster state during split brain
+              ;; woooooooo
+              (c/on-many (:nodes test) (wait 1000 :green))
+              (info "Elasticsearch reports green, but it's probably lying, so waiting another 200s")
+              (Thread/sleep (* 200 1000))
+              (info "Recovered; flushing index before read")
+              (esi/flush client index-name)
+              (assoc op :type :ok
+                     :value (doto (->> (all-results client index-name "number")
+                                       (map (comp :num :_source))
+                                       (into (sorted-set)))
+                              (log/info :results)))
+              (catch RuntimeException e
+                (assoc op :type :fail :value (.getMessage e))))))
 
   (teardown! [_ test]
     (.close client)))
@@ -200,19 +204,19 @@
   []
   (CreateSetClient. nil))
 
-; Use ElasticSearch MVCC to do CAS read/write cycles, implementing a set.
+;; Use ElasticSearch MVCC to do CAS read/write cycles, implementing a set.
 (defrecord CASSetClient [mapping-type doc-id client]
   client/Client
   (setup! [_ test node]
     (let [; client (es/connect [[(name node) 9300]])]
           client (es/connect (str "http://" (name node) ":9200"))]
-        ; Create index
+      ;; Create index
       (try
         (esi/create client index-name
                     :mappings {mapping-type {:properties {}}})
         (catch clojure.lang.ExceptionInfo e
-            ; Is this seriously how you're supposed to do idempotent
-            ; index creation? I've gotta be doing this wrong.
+          ;; Is this seriously how you're supposed to do idempotent
+          ;; index creation? I've gotta be doing this wrong.
           (let [err (http-error e)]
             (when-not (re-find #"IndexAlreadyExistsException" err)
               (throw (RuntimeException. err))))))
@@ -224,43 +228,43 @@
 
   (invoke! [this test op]
     (case (:f op)
-        :add (timeout 5000 (assoc op :type :info :value :timed-out)
-                      (let [current (esd/get client index-name mapping-type doc-id
-                                                :preference "_primary")]
-                        (if-not (esr/found? current)
-                          ; Can't write without a read
-                          (assoc op :type :fail)
+      :add (timeout 5000 (assoc op :type :info :value :timed-out)
+                    (let [current (esd/get client index-name mapping-type doc-id
+                                           :preference "_primary")]
+                      (if-not (esr/found? current)
+                        ;; Can't write without a read
+                        (assoc op :type :fail)
 
-                          (let [version (:_version current)
-                                values  (-> current :_source :values)
-                                values' (vec (conj values (:value op)))
-                                r       (esd/put client index-name mapping-type doc-id
-                                                 {:values values'}
-                                                 :version version)]
-                            (cond ; lol esr/ok? actually means :found, and
-                                  ; esr/found? means :ok or :found, and
-                                  ; *neither* of those appear in a successful
-                                  ; conditional put, so I hope this is how
-                                  ; you're supposed to interpret responses.
-                                  (esr/conflict? r)
-                                  (assoc op :type :fail)
+                        (let [version (:_version current)
+                              values  (-> current :_source :values)
+                              values' (vec (conj values (:value op)))
+                              r       (esd/put client index-name mapping-type doc-id
+                                               {:values values'}
+                                               :version version)]
+                          (cond ; lol esr/ok? actually means :found, and
+                           ;; esr/found? means :ok or :found, and
+                           ;; *neither* of those appear in a successful
+                           ;; conditional put, so I hope this is how
+                           ;; you're supposed to interpret responses.
+                           (esr/conflict? r)
+                           (assoc op :type :fail)
 
-                                  (esr/timed-out? r)
-                                  (assoc op :type :info :value :timed-out)
+                           (esr/timed-out? r)
+                           (assoc op :type :info :value :timed-out)
 
-                                  :else (assoc op :type :ok))))))
+                           :else (assoc op :type :ok))))))
 
-        :read (try
-                (info "Waiting for recovery before read")
-                (c/on-many (:nodes test) (wait 200 :green))
-                (info "Recovered; flushing index before read")
-                (esi/flush client index-name)
-                (assoc op :type :ok
-                       :value (->> (esd/get client index-name mapping-type doc-id
-                                       :preference "_primary")
-                                   :_source
-                                   :values
-                                   (into (sorted-set)))))))
+      :read (try
+              (info "Waiting for recovery before read")
+              (c/on-many (:nodes test) (wait 200 :green))
+              (info "Recovered; flushing index before read")
+              (esi/flush client index-name)
+              (assoc op :type :ok
+                     :value (->> (esd/get client index-name mapping-type doc-id
+                                          :preference "_primary")
+                                 :_source
+                                 :values
+                                 (into (sorted-set)))))))
 
   (teardown! [_ test]
     (.close client)))
--- elasticsearch/test/jepsen/system/elasticsearch_test.clj
+++ elasticsearch/test/jepsen/system/elasticsearch_test.clj
@@ -15,74 +15,74 @@
             [jepsen.store     :as store]
             [jepsen.report    :as report]))
 
-;(deftest register-test
-;  (let [test (run!
-;               (assoc
-;                 noop-test
-;                 :name      "elasticsearch"
-;                 :os        debian/os
-;                 :db        db
-;                 :client    (cas-set-client)
-;                 :model     (model/set)
-;                 :checker   (checker/compose {:html timeline/html
-;                                              :set  checker/set})
-;                 :nemesis   (nemesis/partitioner nemesis/bridge)
-;                 :generator (gen/phases
-;                              (->> (range)
-;                                   (map (fn [x] {:type  :invoke
-;                                                 :f     :add
-;                                                 :value x}))
-;                                   gen/seq
-;                                   (gen/stagger 1/10)
-;                                   (gen/delay 1)
-;                                   (gen/nemesis
-;                                     (gen/seq
-;                                       (cycle [(gen/sleep 60)
-;                                               {:type :info :f :start}
-;                                               (gen/sleep 300)
-;                                               {:type :info :f :stop}])))
-;                                   (gen/time-limit 600))
-;                              (gen/nemesis
-;                                (gen/once {:type :info :f :stop}))
-;                              (gen/clients
-;                                (gen/once {:type :invoke :f :read})))))]
-;    (is (:valid? (:results test)))
-;    (pprint (:results test))))
+;;(deftest register-test
+;;  (let [test (run!
+;;               (assoc
+;;                 noop-test
+;;                 :name      "elasticsearch"
+;;                 :os        debian/os
+;;                 :db        db
+;;                 :client    (cas-set-client)
+;;                 :model     (model/set)
+;;                 :checker   (checker/compose {:html timeline/html
+;;                                              :set  checker/set})
+;;                 :nemesis   (nemesis/partitioner nemesis/bridge)
+;;                 :generator (gen/phases
+;;                              (->> (range)
+;;                                   (map (fn [x] {:type  :invoke
+;;                                                 :f     :add
+;;                                                 :value x}))
+;;                                   gen/seq
+;;                                   (gen/stagger 1/10)
+;;                                   (gen/delay 1)
+;;                                   (gen/nemesis
+;;                                     (gen/seq
+;;                                       (cycle [(gen/sleep 60)
+;;                                               {:type :info :f :start}
+;;                                               (gen/sleep 300)
+;;                                               {:type :info :f :stop}])))
+;;                                   (gen/time-limit 600))
+;;                              (gen/nemesis
+;;                                (gen/once {:type :info :f :stop}))
+;;                              (gen/clients
+;;                                (gen/once {:type :invoke :f :read})))))]
+;;    (is (:valid? (:results test)))
+;;    (pprint (:results test))))
 
 (deftest create-test
   (let [test (run!
-               (assoc
-                 noop-test
-                 :name      "elasticsearch"
-                 :os        debian/os
-                 :db        db
-                 :client    (create-set-client)
-                 :model     (model/set)
-                 :checker   (checker/compose {:html timeline/html
-                                              :set  checker/set})
-                 :nemesis   isolate-self-primaries-nemesis
-                 ;:nemesis   (nemesis/partition-random-node)
-                 ;:nemesis   (nemesis/partition-random-halves)
-                 ;:nemesis   (nemesis/partition-halves)
-                 ;:nemesis   (nemesis/partitioner nemesis/bridge)
-                 :generator (gen/phases
-                              (->> (range)
-                                   (map (fn [x] {:type  :invoke
-                                                 :f     :add
-                                                 :value x}))
-                                   gen/seq
-                                   (gen/stagger 1/10)
-                                   (gen/delay 1)
-                                   (gen/nemesis
-                                     (gen/seq
-                                         [(gen/sleep 30)
-                                          {:type :info :f :start}
-                                          (gen/sleep 200)
-                                          {:type :info :f :stop}]))
-                                   (gen/time-limit 300))
-                              (gen/nemesis
-                                (gen/once {:type :info :f :stop}))
-                              (gen/clients
-                                (gen/once {:type :invoke :f :read})))))]
+              (assoc
+                  noop-test
+                :name      "elasticsearch"
+                :os        debian/os
+                :db        db
+                :client    (create-set-client)
+                :model     (model/set)
+                :checker   (checker/compose {:html timeline/html
+                                             :set  checker/set})
+                :nemesis   isolate-self-primaries-nemesis
+                ;;:nemesis   (nemesis/partition-random-node)
+                ;;:nemesis   (nemesis/partition-random-halves)
+                ;;:nemesis   (nemesis/partition-halves)
+                ;;:nemesis   (nemesis/partitioner nemesis/bridge)
+                :generator (gen/phases
+                            (->> (range)
+                                 (map (fn [x] {:type  :invoke
+                                              :f     :add
+                                              :value x}))
+                                 gen/seq
+                                 (gen/stagger 1/10)
+                                 (gen/delay 1)
+                                 (gen/nemesis
+                                  (gen/seq
+                                   [(gen/sleep 30)
+                                    {:type :info :f :start}
+                                    (gen/sleep 200)
+                                    {:type :info :f :stop}]))
+                                 (gen/time-limit 300))
+                            (gen/nemesis
+                             (gen/once {:type :info :f :stop}))
+                            (gen/clients
+                             (gen/once {:type :invoke :f :read})))))]
     (is (:valid? (:results test)))
     (pprint (:results test))))
